5b58f7bb4dddaca42e536aef098a6e957f0420f7
Niang Codeur <niangpro2405@gmail.com>
2025-10-08 21:39:32 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB')
+})
 
 const app = express()
 
@@ -6,3 +14,4 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000')
 }
 )
+
