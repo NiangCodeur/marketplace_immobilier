02f092e61227c8195d77fd871461e2c128feaeff
Niang Codeur <niangpro2405@gmail.com>
2025-10-18 10:17:36 +0000

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -48,4 +48,14 @@ const start = async () => {
 
 start()
 
+// create a middleware and a function to handle possible errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500 
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
 
